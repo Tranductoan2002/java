b5a859a2282b84ae8a0de74946364d7704d2e949
hoi dan it <tranductoan12c1124@gmail.com>
2024-05-22 00:18:35 +0700

    init project

---
+++ src/testjava/HeloWorld.java
@@ -0,0 +1,11 @@
+package testjava;
+
+public class HeloWorld {
+	public static void main(String[] args) {
+		System.out.println("helo toàn đến với java");
+		System.out.println("helo toàn đến với java");
+		System.out.println("helo toàn đến với java");
+		System.out.println("helo toàn đến với java");
+	}
+
+}
